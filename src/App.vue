f13133fd87f9e785670d335af633aadc3c7a5df0
António Rocha <afgrocha98@gmail.com>
2023-12-27 19:36:16 +0000

    Added TI Logo

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
+  <img class="team-icarus" src="./assets/Icarus1.png" />
   <el-tabs v-model="activeTab" class="demo-tabs" @tab-click="handleClick">
-    <el-tab-pane label="Top 3" name="top3"/>
-    <el-tab-pane label="Thumbnails" name="thumbnails"/>
+    <el-tab-pane label="Top 3" name="top3" />
+    <el-tab-pane label="Thumbnails" name="thumbnails" />
   </el-tabs>
-  <Top3 v-show="activeTab ==='top3'"/>
+  <Top3 v-show="activeTab === 'top3'" />
 </template>
 
 <script>
-import Top3 from './components/Top3.vue'
+import Top3 from "./components/Top3.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Top3
+    Top3,
   },
   data() {
     return {
-      activeTab: 'top3'
-    }
+      activeTab: "top3",
+    };
   },
   methods: {
     handleClick() {
-      console.log(this.activeTab)
-    }
-  }
-}
+      console.log(this.activeTab);
+    },
+  },
+};
 </script>
 
 <style>
@@ -41,6 +42,11 @@ export default {
 }
 
 .el-form-item__label {
-  color: white !important; 
+  color: white !important;
+}
+
+.team-icarus {
+  width: 70px;
+  float: right;
 }
 </style>
